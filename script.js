33b564e18abbb07ee9f7a8fc2b4dcbcfbe81ea6d
ShotRep <78008209+ShotRep@users.noreply.github.com>
2021-07-07 12:03:14 -0500

    Initial commit for this events section.

---
+++ script.js
@@ -0,0 +1,8 @@
+//addEventListener
+//specify the event type and a callback to run.
+
+const button = document.querySelector("h1")
+
+buttom.addEventListener("click", () => {
+  alert("I am a mother lover!")
+})
