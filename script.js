eb4c5d95854f5d3decf23f7626887641481405eb
ShotRep <78008209+ShotRep@users.noreply.github.com>
2021-07-08 12:11:02 -0500

    completed the events multiple elements section

--- script.js
+++ script.js
@@ -102,17 +102,81 @@ const colors = [
 // }
 
 /////////////3rd\\\using this (box)\\\change font color\\\\\\\\\\\\\\\
-const changeColor = function (box) {
-  const pac = document.querySelector(".pac")
-  pac.style.color = this.style.backgroundColor
-}
+// const changeColor = function (box) {
+//   const pac = document.querySelector(".pac")
+//   pac.style.color = this.style.backgroundColor
+// }
 
-const container = document.querySelector("#boxes")
+// const container = document.querySelector("#boxes")
 
-for (let color of colors) {
-  const box = document.createElement("div")
-  box.style.backgroundColor = color
-  box.classList.add("box")
-  container.appendChild(box)
-  box.addEventListener("click", changeColor)
-}
+// for (let color of colors) {
+//   const box = document.createElement("div")
+//   box.style.backgroundColor = color
+//   box.classList.add("box")
+//   container.appendChild(box)
+//   box.addEventListener("click", changeColor)
+// }
+
+//////////The Event Object\\\\\\\\\\\\\\
+//we have access to the objects of event listeners
+//inside the keypress event we have access to the key pressed
+// document.body.addEventListener("keypress", (event) => {
+//   alert(event.key)
+// })
+
+///////////Events - keys\\\\\\\\\\\\\\\\\\\
+
+// const input = document.querySelector("#username")
+
+// input.addEventListener("keydown", (event) => {
+//   console.log('KEY DOWN!')
+//   })
+
+// input.addEventListener("keyup", (event) => {
+//   console.log('KEY UP!')
+// })
+
+// input.addEventListener("keypress", (event) => {
+//   console.log("KEY WAS PRESSED!")
+// })
+
+/////////////////////////////////////////////////////////////////////
+//////// HTML items with id's we made 
+// <input type="text" id="addItem" placeholder="food">  INPUT
+// <ul id="items"></ul>                                 UL
+
+//////// Select the HTML items by id
+// const addItemInput = document.querySelector("#addItem")
+// const itemsUL = document.querySelector("#items")
+
+// // add event listener
+// addItemInput.addEventListener("keypress", function(e){   // <=cannot use arrow functions with this
+//   if (e.key === 'Enter') {                               // when we hit the enter key
+//     const newItemText = this.value                       // capture txt in input
+//     const newItem = document.createElement('LI')         // create new li for appending to ul
+//     newItem.innerText = newItemText                      // change li txt to captured value
+//     itemsUL.appendChild(newItem)                         // append the li to ul
+//     this.value = ''                                      // clears input after we hit enter
+// }
+// })
+
+
+const newInput = document.querySelector('#addItem')
+const newLI = document.querySelector('#items')
+
+newInput.addEventListener('keypress', function (e) {
+  if (e.key === 'Enter') {
+    const newItemText = this.value
+    const newItem = document.createElement('li')
+    newItem.innerText = newItemText
+    newLI.appendChild(newItem)
+    this.value = ''
+  }
+})
+
+/*
+1.) const variable = document.querySelector('')
+2.) variable.addEventListener(''){ }
+3.) newElement = document.createElement('')
+4.) newElement.appendChild
+*/
